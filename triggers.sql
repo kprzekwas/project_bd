6205b708db4421df946122623081de094786ed56
Kacper Przekwas <kacper@MacBook-Air-Kacper.local>
2018-06-10 00:09:39 +0200

    Trigger example added

---
+++ triggers.sql
@@ -0,0 +1,12 @@
+CREATE TRIGGER sprawdz_date_przyjecia
+ON pojazd 
+FOR INSERT
+AS
+DECLARE
+	@data_przyjecia DATE;
+BEGIN
+	SELECT @data_przyjecia = data_przyjecia FROM pojazd;
+	IF @data_przyjecia > GETDATE()
+		RAISERROR('Niewłaściwa data przyjęcia!', 1, 2)
+END
+ROLLBACK
